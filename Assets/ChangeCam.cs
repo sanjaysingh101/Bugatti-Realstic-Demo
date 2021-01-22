2010ad2e3b220538edb0a0f8f1b71b57e4dc96ca
sanjay6939 <sanjaysingh6939@gmail.com>
2021-01-22 09:55:04 -0800

    Project Commit
    0-400-0

---
+++ Assets/ChangeCam.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCam : MonoBehaviour
+{
+    public GameObject sideCam;
+    public GameObject ThirdCam;
+    public GameObject UpCam;
+    public GameObject Dolly;
+    // Start is called before the first frame update
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("1"))
+        {
+            ThirdCam.SetActive(true);
+            sideCam.SetActive(false);
+
+        }
+        if (other.CompareTag("2"))
+        {
+            ThirdCam.SetActive(false);
+            sideCam.SetActive(false);
+            Dolly.SetActive(true);
+
+        }
+    }
+
+
+}
